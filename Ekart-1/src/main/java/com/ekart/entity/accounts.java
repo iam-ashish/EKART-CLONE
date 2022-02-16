43e695c1b6fd1406c2b15e817062717c29f91f8d
vishnu0044 <priyashekar1203@gmail.com>
2022-02-16 23:53:36 +0530

    DB Table Entity

---
+++ Ekart-1/src/main/java/com/ekart/entity/accounts.java
@@ -0,0 +1,14 @@
+package com.ekart.entity;
+
+
+public class accounts {
+
+	private String user_name;
+	
+	private String active;
+	
+	private String encrypted_password;
+	
+	private String user_role;
+	
+}
